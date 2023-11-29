cfb33b5325ba6401c39321cefdbffb8af58882a9
Harsh RB <harshrb2424@gmail.com>
2023-11-29 23:52:57 +0530

    % calculations

--- public/index.js
+++ public/index.js
@@ -2,12 +2,9 @@ $(document).ready(function () {
   // Fetch data from CSM-info.json and attendance.json
   $.getJSON("CSM-info.json", function (students) {
     $.getJSON("attendance.json", function (attendanceData) {
-
-
-
       $.each(students, function (_, student) {
         $("#attendanceTable tbody").append(
-          '<tr id="' +
+          '<tr class="student" id="' +
             student["roll-number"] +
             '">' +
             "<td>" +
@@ -18,74 +15,33 @@ $(document).ready(function () {
             "</td>" +
             "</tr>"
         );
-
-
       });
-
       $.each(attendanceData, function (_, attendance) {
-        console.log(attendance);
-
-        
         if (attendance["college"] == "Working") {
             $("#attendanceTable thead tr").append(
               "<th>" + attendance["date"] + "</th>"
             );
             $.each(students, function (_, student) {
-                console.log(attendance["student"]);
-                console.log(student);
-
                 if (attendance["student"].includes(student['roll-number'])) {
-                    console.log(`${student['roll-number']} is in the array.`);
                     $("#"+student['roll-number']).append(
                         "<th>" + "P" + "</th>"
                       );
                 } else {
-                    console.log(`${student['roll-number']} is not in the array.`);
                     $("#"+student['roll-number']).append(
                         "<th>" + "A" + "</th>"
                       );
                 }
-
               });
+
         }
-
       });
-      // Iterate through each attendance record
-      // $.each(attendanceData, function (index, record) {
-      //     // Check if the record has student attendance data
-      //     if (record.student) {
-      //         // Iterate through each student in the record
-      //         $.each(record.student, function (_, rollNumber) {
-      //             // Find the student information using the roll number
-      //             var studentInfo = students.find(function (student) {
-      //                 return student['roll-number'] === rollNumber;
-      //             });
-
-      //             // Add a row to the table with attendance details
-      //             if (studentInfo) {
-      //                 $('#attendanceTable tbody').append(
-      //                     '<tr>' +
-      //                     '<td>' + record.date + '</td>' +
-      //                     '<td>' + record.college + '</td>' +
-      //                     '<td>' + rollNumber + '</td>' +
-      //                     '<td>' + studentInfo.name + '</td>' +
-      //                     '</tr>'
-      //                 );
-      //             }
-      //         });
-      //     } else {
-      //         // Add a row for holiday
-      //         $('#attendanceTable tbody').append(
-      //             '<tr>' +
-      //             '<td>' + record.date + '</td>' +
-      //             '<td>' + record.college + '</td>' +
-      //             '<td colspan="2">Holiday</td>' +
-      //             '</tr>'
-      //         );
-      //     }
-      // });
+      $('thead tr').prepend('<th>'+"%"+'</th>');
+      $('tr.student').each(function() {
+        const total_days = $(this).find('th').length;
+        const present_days = $(this).find('th:contains("P")').length;
+        $(this).prepend('<td>'+(present_days/total_days*100)+'</td>');
+    });
     });
   });
-
 
 });
